82e8314771a401ab19151dd9b56025b70233a178
jpromanonet <programming@jpromano.net>
2023-03-20 20:18:09 -0300

    Day 04

--- 04_Update_Youtube_Channel_Descriptions/main.py
+++ 04_Update_Youtube_Channel_Descriptions/main.py
@@ -7,3 +7,14 @@ def upload_video(video_file, video_title, video_description):
     credentials, project_id = google.auth.default()
     youtube = googleapiclient.discovery.build("youtube", "v3", credentials=credentials)
 
+    # Define the video metadata
+    video_metadata = {
+        "snippet": {
+            "title": video_title,
+            "description": video_description,
+            "categoryId": 22
+        },
+        "status": {
+            "privacyStatus": "private"
+        }
+    }

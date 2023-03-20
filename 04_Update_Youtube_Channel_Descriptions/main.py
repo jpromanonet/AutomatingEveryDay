7b60d54d44645655a8ff372c0297c81ccdbc8903
jpromanonet <programming@jpromano.net>
2023-03-20 20:18:03 -0300

    Day 04

--- 04_Update_Youtube_Channel_Descriptions/main.py
+++ 04_Update_Youtube_Channel_Descriptions/main.py
@@ -2,4 +2,8 @@ import os
 import google.auth
 import googleapiclient.discovery
 
+def upload_video(video_file, video_title, video_description):
+    # Authenticate and construct the YouTube API client
+    credentials, project_id = google.auth.default()
+    youtube = googleapiclient.discovery.build("youtube", "v3", credentials=credentials)
 

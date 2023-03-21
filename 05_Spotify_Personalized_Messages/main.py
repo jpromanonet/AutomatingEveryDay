2f173c996e439290af33fd165bf16e8fb4aaf4b0
jpromanonet <programming@jpromano.net>
2023-03-21 19:42:14 -0300

    Day 05

--- 05_Spotify_Personalized_Messages/main.py
+++ 05_Spotify_Personalized_Messages/main.py
@@ -2,6 +2,11 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import random
 
+# Set up authentication using the Spotify API credentials
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="your_client_id",
+                                               client_secret="your_client_secret",
+                                               redirect_uri="your_redirect_uri",
+                                               scope="user-read-recently-played user-top-read"))
 
 
 

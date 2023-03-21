18bd493c8ca216ed028206d9a852d776c5d354f6
jpromanonet <programming@jpromano.net>
2023-03-21 19:42:31 -0300

    Day 05

--- 05_Spotify_Personalized_Messages/main.py
+++ 05_Spotify_Personalized_Messages/main.py
@@ -11,6 +11,8 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="your_client_id",
 # Get the user's recently played tracks
 recent_tracks = sp.current_user_recently_played(limit=50)
 
+# Get the user's top artists
+top_artists = sp.current_user_top_artists(limit=10, time_range='medium_term')
 
 
 

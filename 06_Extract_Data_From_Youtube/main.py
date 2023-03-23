ec785c678d301545732489287576d9957f934ee7
jpromanonet <programming@jpromano.net>
2023-03-22 23:28:41 -0300

    Day 06

--- 06_Extract_Data_From_Youtube/main.py
+++ 06_Extract_Data_From_Youtube/main.py
@@ -4,5 +4,7 @@ import json
 # Replace with your own API key
 api_key = "your_api_key_here"
 
+# YouTube Data API endpoint for searching for videos
+search_endpoint = "https://www.googleapis.com/youtube/v3/search"
 
 

4338bb37d29052beb2fcc3cb287467fc19ceb8fa
jpromanonet <programming@jpromano.net>
2023-03-22 23:28:49 -0300

    Day 06

--- 06_Extract_Data_From_Youtube/main.py
+++ 06_Extract_Data_From_Youtube/main.py
@@ -7,4 +7,14 @@ api_key = "your_api_key_here"
 # YouTube Data API endpoint for searching for videos
 search_endpoint = "https://www.googleapis.com/youtube/v3/search"
 
+# Function to search for videos based on a keyword or phrase
+def search_videos(q):
+    params = {
+        "q": q,
+        "part": "snippet",
+        "type": "video",
+        "key": api_key
+    }
+    response = requests.get(search_endpoint, params=params)
+    return response.json()
 

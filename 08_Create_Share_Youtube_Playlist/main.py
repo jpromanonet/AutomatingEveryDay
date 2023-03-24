17198f253a7edf03bce3149b5338d009080e7429
jpromanonet <programming@jpromano.net>
2023-03-24 20:52:57 -0300

    Day 08

--- 08_Create_Share_Youtube_Playlist/main.py
+++ 08_Create_Share_Youtube_Playlist/main.py
@@ -3,3 +3,6 @@ import json
 
 # Replace with your own API key
 api_key = "your_api_key_here"
+
+# YouTube Data API endpoint for searching for videos
+search_endpoint = "https://www.googleapis.com/youtube/v3/search"

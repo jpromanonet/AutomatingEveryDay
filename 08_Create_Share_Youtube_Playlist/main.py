a439ef1ab35a82f3eabf43c12e68df733934f86f
jpromanonet <programming@jpromano.net>
2023-03-24 20:54:46 -0300

    Day 08

--- 08_Create_Share_Youtube_Playlist/main.py
+++ 08_Create_Share_Youtube_Playlist/main.py
@@ -44,3 +44,16 @@ def add_video_to_playlist(playlist_id, video_id):
     }
     response = requests.post(playlist_endpoint + "/" + playlist_id + "/items", params={"part": "snippet", "key": api_key}, json=data)
     return response.json()
+
+# Main function to automate the process of creating and sharing YouTube playlists based on specific genres or themes
+def main():
+    # Example code to search for videos based on a genre or theme
+    genre_or_theme = "music"
+    search_results = search_videos(genre_or_theme)
+
+    # Example code to create a new playlist based on the genre or theme
+    playlist_title = genre_or_theme + " playlist"
+    playlist = create_playlist(playlist_title)
+    playlist_id = playlist["id"]
+
+    # Example code to add the videos you're interested

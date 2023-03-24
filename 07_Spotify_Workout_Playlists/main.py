4f2b39a229096959b96eec33315aedceb17eb2a6
jpromanonet <programming@jpromano.net>
2023-03-23 23:33:54 -0300

    Day 07

--- 07_Spotify_Workout_Playlists/main.py
+++ 07_Spotify_Workout_Playlists/main.py
@@ -4,5 +4,8 @@ import json
 # Replace with your own access token
 access_token = "your_access_token_here"
 
+# Spotify Web API endpoint for searching for tracks
+search_endpoint = "https://api.spotify.com/v1/search"
+
 
 

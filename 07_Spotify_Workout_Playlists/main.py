ed4a30d4f126abc8650eba6df654a654528dbd57
jpromanonet <programming@jpromano.net>
2023-03-23 23:34:03 -0300

    Day 07

--- 07_Spotify_Workout_Playlists/main.py
+++ 07_Spotify_Workout_Playlists/main.py
@@ -10,5 +10,10 @@ search_endpoint = "https://api.spotify.com/v1/search"
 # Spotify Web API endpoint for adding tracks to a playlist
 add_tracks_endpoint = "https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
 
+# API header with the access token
+header = {
+    "Authorization": "Bearer " + access_token
+}
+
 
 

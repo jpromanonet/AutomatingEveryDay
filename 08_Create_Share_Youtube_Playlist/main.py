869f839b04db377007a76a5d71ffdd5a557d6bce
jpromanonet <programming@jpromano.net>
2023-03-24 20:53:04 -0300

    Day 08

--- 08_Create_Share_Youtube_Playlist/main.py
+++ 08_Create_Share_Youtube_Playlist/main.py
@@ -6,3 +6,7 @@ api_key = "your_api_key_here"
 
 # YouTube Data API endpoint for searching for videos
 search_endpoint = "https://www.googleapis.com/youtube/v3/search"
+
+# YouTube Data API endpoint for creating a new playlist
+playlist_endpoint = "https://www.googleapis.com/youtube/v3/playlists"
+

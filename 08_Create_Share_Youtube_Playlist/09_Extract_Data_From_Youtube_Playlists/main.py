b5193b62b7a75361bd18083f15f7a321c92bb4dd
jpromanonet <programming@jpromano.net>
2023-03-25 20:58:26 -0300

    Day 09

--- 08_Create_Share_Youtube_Playlist/09_Extract_Data_From_Youtube_Playlists/main.py
+++ 08_Create_Share_Youtube_Playlist/09_Extract_Data_From_Youtube_Playlists/main.py
@@ -3,3 +3,6 @@ import json
 
 # Replace with your own API key
 api_key = "your_api_key_here"
+
+# YouTube Data API endpoint for retrieving channel information
+channel_endpoint = "https://www.googleapis.com/youtube/v3/channels"

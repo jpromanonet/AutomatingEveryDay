598096dcbd685e6b884cfa47c4d7427da1b94c30
jpromanonet <programming@jpromano.net>
2023-03-25 20:58:39 -0300

    Day 09

--- 08_Create_Share_Youtube_Playlist/09_Extract_Data_From_Youtube_Playlists/main.py
+++ 08_Create_Share_Youtube_Playlist/09_Extract_Data_From_Youtube_Playlists/main.py
@@ -10,3 +10,15 @@ channel_endpoint = "https://www.googleapis.com/youtube/v3/channels"
 # YouTube Data API endpoint for retrieving subscriber information
 subscriber_endpoint = "https://www.googleapis.com/youtube/v3/subscriptions"
 
+# Function to retrieve channel information based on a channel ID or username
+def get_channel_info(channel_id_or_username):
+    params = {
+        "part": "snippet,statistics",
+        "key": api_key
+    }
+    if channel_id_or_username.startswith("UC"):
+        params["id"] = channel_id_or_username
+    else:
+        params["forUsername"] = channel_id_or_username
+    response = requests.get(channel_endpoint, params=params)
+    return response.json()

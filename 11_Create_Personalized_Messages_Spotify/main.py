5df887ad64d1bd0e4dca20a83e44f0603e30224e
jpromanonet <programming@jpromano.net>
2023-03-27 18:13:26 -0300

    Day 14

--- 11_Create_Personalized_Messages_Spotify/main.py
+++ 11_Create_Personalized_Messages_Spotify/main.py
@@ -9,3 +9,7 @@ CLIENT_SECRET = "your_client_secret"
 REDIRECT_URI = "http://localhost:8000"
 SCOPE = "user-read-private user-read-email user-library-read playlist-read-private playlist-modify-public"
 
+# User credentials
+USERNAME = "your_spotify_username"
+PLAYLIST_ID = "your_playlist_id"
+

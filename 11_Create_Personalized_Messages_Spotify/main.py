5832242a399c1b6e03903c561cacd7424fe306cd
jpromanonet <programming@jpromano.net>
2023-03-27 18:13:21 -0300

    Day 14

--- 11_Create_Personalized_Messages_Spotify/main.py
+++ 11_Create_Personalized_Messages_Spotify/main.py
@@ -3,3 +3,9 @@ import spotipy.util as util
 import random
 import time
 
+# Spotify API credentials
+CLIENT_ID = "your_client_id"
+CLIENT_SECRET = "your_client_secret"
+REDIRECT_URI = "http://localhost:8000"
+SCOPE = "user-read-private user-read-email user-library-read playlist-read-private playlist-modify-public"
+

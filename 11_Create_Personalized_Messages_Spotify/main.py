a32ecce14d5a3c1165b2d4a3b4ff79c7d6f17466
jpromanonet <programming@jpromano.net>
2023-03-27 18:13:57 -0300

    Day 14

--- 11_Create_Personalized_Messages_Spotify/main.py
+++ 11_Create_Personalized_Messages_Spotify/main.py
@@ -21,3 +21,15 @@ def get_token(username, client_id, client_secret, redirect_uri, scope):
     token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
     return token
 
+# Function to get playlist tracks
+def get_tracks(username, playlist_id, token):
+    sp = spotipy.Spotify(auth=token)
+    tracks = []
+    offset = 0
+    while True:
+        results = sp.user_playlist_tracks(username, playlist_id, offset=offset)
+        tracks += results['items']
+        offset += len(results['items'])
+        if len(results['items']) == 0:
+            break
+    return tracks

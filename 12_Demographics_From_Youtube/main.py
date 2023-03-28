f640bc80b1bdc64ec8fbcfbea03770db0ccc6b88
jpromanonet <programming@jpromano.net>
2023-03-28 18:19:18 -0300

    Day 15

--- 12_Demographics_From_Youtube/main.py
+++ 12_Demographics_From_Youtube/main.py
@@ -4,3 +4,11 @@ import google.oauth2.credentials
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+# Set API credentials
+API_NAME = "youtube"
+API_VERSION = "v3"
+CLIENT_SECRETS_FILE = "client_secrets.json"
+
+# Set video ID
+VIDEO_ID = "your_video_id"
+

b2f2ce23f1e1ffc9e6af771be07f4354f488ceee
jpromanonet <programming@jpromano.net>
2023-03-28 18:22:54 -0300

    Day 15

--- 12_Demographics_From_Youtube/main.py
+++ 12_Demographics_From_Youtube/main.py
@@ -12,3 +12,21 @@ CLIENT_SECRETS_FILE = "client_secrets.json"
 # Set video ID
 VIDEO_ID = "your_video_id"
 
+# Authenticate API access
+def authenticate():
+    credentials = None
+    try:
+        credentials = google.auth.load_credentials_from_file(
+            "credentials.json",
+            scopes=["https://www.googleapis.com/auth/youtube.readonly"],
+        )
+    except Exception:
+        credentials = None
+    if not credentials or not credentials.valid:
+        flow = google.auth.transport.requests.Request()
+        flow.credentials = google.oauth2.credentials.Credentials.from_authorized_user_file(
+            "credentials.json", scopes=["https://www.googleapis.com/auth/youtube.readonly"]
+        )
+        credentials = flow.credentials
+    return credentials
+

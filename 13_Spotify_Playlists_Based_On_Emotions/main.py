b92be2edfd38630ce018bde788d8c56b3272e170
jpromanonet <programming@jpromano.net>
2023-03-29 18:13:41 -0300

    Day 13

--- 13_Spotify_Playlists_Based_On_Emotions/main.py
+++ 13_Spotify_Playlists_Based_On_Emotions/main.py
@@ -2,3 +2,9 @@ import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from textblob import TextBlob
+
+# Set up Spotify API credentials
+os.environ["SPOTIPY_CLIENT_ID"] = "your_spotify_client_id"
+os.environ["SPOTIPY_CLIENT_SECRET"] = "your_spotify_client_secret"
+os.environ["SPOTIPY_REDIRECT_URI"] = "your_spotify_redirect_uri"
+

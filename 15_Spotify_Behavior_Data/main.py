661133312168db23287c0b6dc72d6588db7f56ab
jpromanonet <programming@jpromano.net>
2023-03-31 19:25:07 -0300

    Day 15

--- 15_Spotify_Behavior_Data/main.py
+++ 15_Spotify_Behavior_Data/main.py
@@ -1,3 +1,8 @@
 import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="YOUR_CLIENT_ID",
+                                               client_secret="YOUR_CLIENT_SECRET",
+                                               redirect_uri="YOUR_REDIRECT_URI",
+                                               scope="user-top-read"))

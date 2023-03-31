37fef3b98aee431d6899b358fe3198b1bad5fd31
jpromanonet <programming@jpromano.net>
2023-03-31 19:25:25 -0300

    Day 15

--- 15_Spotify_Behavior_Data/main.py
+++ 15_Spotify_Behavior_Data/main.py
@@ -6,3 +6,8 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="YOUR_CLIENT_ID",
                                                client_secret="YOUR_CLIENT_SECRET",
                                                redirect_uri="YOUR_REDIRECT_URI",
                                                scope="user-top-read"))
+
+def get_user_top_tracks_and_artists(time_range, limit):
+    top_tracks = sp.current_user_top_tracks(time_range=time_range, limit=limit)
+    top_artists = sp.current_user_top_artists(time_range=time_range, limit=limit)
+    return top_tracks, top_artists

ded707a2df73dc3c8719a9071a51fb9e41cc974d
jpromanonet <programming@jpromano.net>
2023-04-01 19:31:05 -0300

    Day 16

--- 16_Posting_updates_from_RSS/main.py
+++ 16_Posting_updates_from_RSS/main.py
@@ -7,3 +7,13 @@ import googleapiclient.errors
 
 scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+def setup_youtube_api():
+    api_service_name = "youtube"
+    api_version = "v3"
+    client_secrets_file = "client_secret.json"
+
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes)
+    credentials = flow.run_console()
+    youtube = googleapiclient.discovery.build(api_service_name, api_version, credentials=credentials)
+    return youtube
